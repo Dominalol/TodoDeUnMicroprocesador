5dcdcbffd8307a8e9f6bc36dc3779e505150331a
Dominalol <157432928+Dominalol@users.noreply.github.com>
2024-04-22 14:05:08 -0600

    Add files via upload

---
+++ scripts-amd.js
@@ -0,0 +1,40 @@
+
+function verificarProcesador(){
+    if (document.getElementById("amd-seleccion").values=true){
+
+        botonSeleccionado = document.getElementById('amd-seleccion');
+        miVariable = botonSeleccionado.dataset.valor;
+        document.getElementById("mensaje").innerHTML=miVariable // Imprime "valor personalizado"
+        
+    }
+}
+
+function verificarProcesador2(){
+    if (document.getElementById("amd-seleccion2").values=true){
+
+        botonSeleccionado = document.getElementById('amd-seleccion2');
+        miVariable = botonSeleccionado.dataset.valor;
+        document.getElementById("mensaje").innerHTML=miVariable // Imprime "valor personalizado"
+        
+    }
+}
+
+function verificarProcesador3(){
+    if (document.getElementById("amd-seleccion3").values=true){
+
+        botonSeleccionado = document.getElementById('amd-seleccion3');
+        miVariable = botonSeleccionado.dataset.valor;
+        document.getElementById("mensaje").innerHTML=miVariable // Imprime "valor personalizado"
+        
+    }
+}
+
+function verificarProcesador4(){
+    if (document.getElementById("amd-seleccion4").values=true){
+
+        botonSeleccionado = document.getElementById('amd-seleccion4');
+        miVariable = botonSeleccionado.dataset.valor;
+        document.getElementById("mensaje").innerHTML=miVariable // Imprime "valor personalizado"
+        
+    }
+}
