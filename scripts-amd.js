28f0bb04cc485e4c49fbf988d241043549ab7fc7
Dominalol <157432928+Dominalol@users.noreply.github.com>
2024-04-22 21:21:55 -0600

    Add files via upload

--- scripts-amd.js
+++ scripts-amd.js
@@ -38,3 +38,39 @@ function verificarProcesador4(){
         
     }
 }
+function verificarCaracteristicas(){
+    if (document.getElementById("amd-caracteristicas").values=true){
+
+        botonSeleccionado = document.getElementById('amd-caracteristicas');
+        miVariable = botonSeleccionado.dataset.valor;
+        document.getElementById("mensaje").innerHTML=" Aca" // Imprime "valor personalizado"
+        
+    }
+}
+function verificarArquitectura(){
+    if (document.getElementById("amd-arquitectura").values=true){
+
+        botonSeleccionado = document.getElementById('amd-arquitectura');
+        miVariable = botonSeleccionado.dataset.valor;
+        document.getElementById("mensaje").innerHTML=" Aa" // Imprime "valor personalizado"
+        
+    }
+}
+function verificarReview(){
+    if (document.getElementById("amd-review").values=true){
+
+        botonSeleccionado = document.getElementById('amd-review');
+        miVariable = botonSeleccionado.dataset.valor;
+        document.getElementById("mensaje").innerHTML=" Ar" // Imprime "valor personalizado"
+        
+    }
+}
+function verificarComprar(){
+    if (document.getElementById("amd-comprar").values=true){
+
+        botonSeleccionado = document.getElementById('amd-comprar');
+        miVariable = botonSeleccionado.dataset.valor;
+        document.getElementById("mensaje").innerHTML=" Ac" // Imprime "valor personalizado"
+        
+    }
+}
--- scripts.js
+++ scripts.js
@@ -38,3 +38,40 @@ function verificarProcesador4(){
         
     }
 }
+
+function verificarCaracteristicas(){
+    if (document.getElementById("intel-caracteristicas").values=true){
+
+        botonSeleccionado = document.getElementById('intel-caracteristicas');
+        miVariable = botonSeleccionado.dataset.valor;
+        document.getElementById("mensaje").innerHTML=" ca" // Imprime "valor personalizado"
+        
+    }
+}
+function verificarArquitectura(){
+    if (document.getElementById("intel-arquitectura").values=true){
+
+        botonSeleccionado = document.getElementById('intel-arquitectura');
+        miVariable = botonSeleccionado.dataset.valor;
+        document.getElementById("mensaje").innerHTML=" a" // Imprime "valor personalizado"
+        
+    }
+}
+function verificarReview(){
+    if (document.getElementById("intel-review").values=true){
+
+        botonSeleccionado = document.getElementById('intel-review');
+        miVariable = botonSeleccionado.dataset.valor;
+        document.getElementById("mensaje").innerHTML=" r" // Imprime "valor personalizado"
+        
+    }
+}
+function verificarComprar(){
+    if (document.getElementById("intel-comprar").values=true){
+
+        botonSeleccionado = document.getElementById('intel-comprar');
+        miVariable = botonSeleccionado.dataset.valor;
+        document.getElementById("mensaje").innerHTML=" c" // Imprime "valor personalizado"
+        
+    }
+}
