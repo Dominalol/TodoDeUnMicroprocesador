3d34e81cc1673b468b1b8fdf0fa466d0b17d847c
Dominalol <157432928+Dominalol@users.noreply.github.com>
2024-06-10 19:21:11 -0600

    Delete script-frame-intel.js

--- script-frame-intel.js
+++
@@ -1,9 +0,0 @@
-const procesadorSelect = document.getElementById('procesador');                 //Obtiene el objeto seleccionado
-
-procesadorSelect.addEventListener('change', function() {                        //Adjunta un detector de eventos al evento change del elemento seleccionado
-    const procesadorSeleccionado = procesadorSelect.value;                      //Recupera el valor del elemento seleccionado
-    console.log('Procesador Seleccionado:', procesadorSeleccionado);            //Registra el valor seleccionado en la consola para mostrarlo en pantalla
-
-    const infoProcesador = document.getElementById('processor-info');           
-    infoProcesador.textContent = `Seleccionaste: ${procesadorSeleccionado}`;    //Actualiza el contenido de texto del párrafo para mostrar el procesador seleccionado
-});
